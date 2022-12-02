7bdfb1b645a07a59aff214f9d9a0a7493a1a43eb
Jesper Christ <jesperchrist1101@gmail.com>
2022-12-02 13:57:15 +0100

    clean-up

--- file.sql
+++ file.sql
@@ -1,4 +1,5 @@
-CREATE TABLE mitglieder (
+USE beehive;
+CREATE TABLE IF NOT EXISTS mitglieder (
   Mietgliedsnr TINYINT PRIMARY KEY auto_increment,
   Vorname char(20) NOT NULL,
   Nachname char(20) NOT NULL,
@@ -12,20 +13,18 @@ CREATE TABLE mitglieder (
   IBAN int,
   Beitrittsdatum DATE NOT NULL
 );
-CREATE TABLE mitarbeiter (
+CREATE TABLE IF NOT EXISTS mitarbeiter (
   Anfangsdatum DATE NOT NULL,
   Stundenlohn int NOT NULL,
   Steuerklasse int NOT NULL,
   Rolle char(12) NOT NULL
 );
-CREATE TABLE spielfelder (
-  Spielfeldnummer TINYINT PRIMARY KEY auto_increment,
-);
-CREATE TABLE sportarten (
+CREATE TABLE IF NOT EXISTS sportarten (
   SportartNummer TINYINT PRIMARY KEY auto_increment,
   Name char(20),
   Equipment char(50)
-) CREATE TABLE spielfelder (
+);
+CREATE TABLE IF NOT EXISTS spielfelder (
   Spielfeldnummer TINYINT PRIMARY KEY auto_increment,
   Untergrund char(12),
   Markierungen char(12),
@@ -33,7 +32,8 @@ CREATE TABLE sportarten (
   Länge int,
   Breite int,
   Ort char(30)
-) CREATE TABLE teams (
+);
+CREATE TABLE IF NOT EXISTS teams (
   Teamnummer TINYINT PRIMARY KEY auto_increment,
   Bedingungen char(20),
   Altersklasse int,

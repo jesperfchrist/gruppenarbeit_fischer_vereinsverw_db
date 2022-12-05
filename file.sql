aa59809987430788cd56732147353ea97c5e96d8
Jesper Christ <jesperchrist1101@gmail.com>
2022-12-06 00:04:54 +0100

    added comments

--- file.sql
+++ file.sql
@@ -1,4 +1,8 @@
+-- Die grundlegende MySQL Datenbank erstellen
+CREATE DATABASE beehive;
+-- Sicherstellen, dass die Datenbank ausgewählt ist
 USE beehive;
+-- Die Mitgliedertabelle erstmal ohne Foreign Keys
 CREATE TABLE IF NOT EXISTS mitglieder (
   Mietgliedsnummer TINYINT PRIMARY KEY auto_increment,
   Vorname char(20) NOT NULL,
@@ -13,6 +17,7 @@ CREATE TABLE IF NOT EXISTS mitglieder (
   IBAN int,
   Beitrittsdatum DATE NOT NULL
 );
+-- MITARBEITER
 CREATE TABLE IF NOT EXISTS mitarbeiter (
   Anfangsdatum DATE NOT NULL,
   Stundenlohn int NOT NULL,
@@ -21,11 +26,13 @@ CREATE TABLE IF NOT EXISTS mitarbeiter (
   Mietgliedsnummer TINYINT,
   FOREIGN KEY (Mietgliedsnummer) REFERENCES mitglieder(Mietgliedsnummer)
 );
+-- SPORTARTEN
 CREATE TABLE IF NOT EXISTS sportarten (
   Sportartnummer TINYINT PRIMARY KEY auto_increment,
   SportartName char(20),
   Equipment char(50)
 );
+-- SPIELFELDER
 CREATE TABLE IF NOT EXISTS spielfelder (
   Spielfeldnummer TINYINT PRIMARY KEY auto_increment,
   Untergrund char(12),
@@ -35,30 +42,35 @@ CREATE TABLE IF NOT EXISTS spielfelder (
   Breite int,
   Ort char(30)
 );
+-- TEAMS
 CREATE TABLE IF NOT EXISTS teams (
   Teamnummer TINYINT PRIMARY KEY auto_increment,
   Bedingungen char(20),
   Altersklasse int,
   Groesse int
 );
+-- Beziehung: mitglieder spielen in teams
 CREATE TABLE IF NOT EXISTS mitglieder_spielen_in (
   Teamnummer TINYINT,
   Mietgliedsnummer TINYINT,
   FOREIGN KEY (Mietgliedsnummer) REFERENCES mitglieder (Mietgliedsnummer),
   FOREIGN KEY (Teamnummer) REFERENCES teams (Teamnummer)
 );
+-- Beziehung: teams spielen sportarten
 CREATE TABLE IF NOT EXISTS teams_spielen (
   Sportartnummer TINYINT,
   Teamnummer TINYINT,
   FOREIGN KEY (Sportartnummer) REFERENCES sportarten (Sportartnummer),
   FOREIGN KEY (Teamnummer) REFERENCES teams (Teamnummer)
 );
+-- Beziehung: sportarten benötigen spielfelder
 CREATE TABLE IF NOT EXISTS sportarten_benoetigen (
   Sportartnummer TINYINT,
   Spielfeldnummer TINYINT,
   FOREIGN KEY (Sportartnummer) REFERENCES sportarten (Sportartnummer),
   FOREIGN KEY (Spielfeldnummer) REFERENCES spielfelder (Spielfeldnummer)
 );
+-- Beziehung:  mitglieder können sportarten
 CREATE TABLE IF NOT EXISTS mitglieder_können_sportarten (
   Mietgliedsnummer TINYINT,
   Sportartnummer TINYINT,
